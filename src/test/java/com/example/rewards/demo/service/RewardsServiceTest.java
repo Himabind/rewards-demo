830fd4589dd82a5d48fe5570a60a6ae98757218b
Hima Pothana <hima.pothana@gmail.com>
2022-10-05 20:10:24 -0400

    ffirst commit

---
+++ src/test/java/com/example/rewards/demo/service/RewardsServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.rewards.demo.service;
+
+import com.example.rewards.demo.entity.Transaction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RewardsServiceTest {
+
+
+    @Autowired
+    private RewardsService rewardsService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void whenValidCustomerId_thenFindTransactions(){
+        Integer customerId = 1;
+        List<Transaction> list = rewardsService.fetchTransactionListByCustomerId(customerId);
+
+        assertNotNull(list);
+
+    }
+
+
+}
